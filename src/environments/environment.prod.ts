d36844f5c12bfaeb490d83d328ef8ac1111c0a2f
Sept <sszheng3@gmail.com>
2024-07-09 01:36:19 -0400

    added the services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,29 @@
+const baseUrl = window.location.origin;
+
 export const environment = {
   production: true,
-  domain: 'https://apiv1.sszzz.me/api'
+  domain: 'https://apiv1.sszzz.me/api',
+  application: {
+    baseUrl,
+    name: 'Cloud',
+    logoUrl: ''
+  },
+  oAuthConfig: {
+    issuer: 'https://api.cloud.sszzz.me/',
+    redirectUri: baseUrl,
+    responseType: 'password',
+    clientId: 'Cloud_App',
+    // dummyClientSecret: '1q2w3e*',
+    scope: 'offline_access Cloud'
+    // requireHttps: false,
+
+  },
+  apis: {
+    default: {
+      url: 'https://api.cloud.sszzz.me',
+      rootNamespace: 'Davrat.Cloud'
+    }
+  }
 };
 
 // export const environment = {
