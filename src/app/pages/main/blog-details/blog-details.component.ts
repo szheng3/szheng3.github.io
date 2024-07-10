69dd186617e9e9042b924fd2a4c76f088af23c26
Sept <sszheng3@gmail.com>
2024-07-09 22:58:52 -0400

    blogs details ++

--- src/app/pages/main/blog-details/blog-details.component.ts
+++ src/app/pages/main/blog-details/blog-details.component.ts
@@ -1,12 +1,43 @@
 import {Component, OnInit} from '@angular/core';
+import {BlogDto} from "~/proxy/resumes";
+import {ActivatedRoute, RouterLink} from "@angular/router";
+import {BlogService} from "~/core/api/blog.service";
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
+import {LazyLoadImageModule} from "ng-lazyload-image";
 
 @Component({
   selector: 'app-blog-details',
   templateUrl: './blog-details.component.html',
+  standalone: true,
+  imports: [
+    NgIf,
+    DatePipe,
+    NgForOf,
+    RouterLink,
+    LazyLoadImageModule
+  ],
   styleUrls: ['./blog-details.component.scss']
 })
 export class BlogDetailsComponent implements OnInit {
-  constructor() {}
+  blog: BlogDto | undefined;
 
-  ngOnInit() {}
+  constructor(
+    private route: ActivatedRoute,
+    private blogService: BlogService
+  ) {
+  }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.blogService.getOne(id).subscribe(
+        (blog: BlogDto) => {
+          this.blog = blog;
+        },
+        error => {
+          console.error('Error fetching blog post:', error);
+        }
+      );
+    }
+  }
 }
