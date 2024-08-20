c52a5f4981c446916d3541126b3c239ae32ea853
Sept <sszheng3@gmail.com>
2024-08-19 23:01:29 -0400

    added the share function works

--- src/app/pages/main/blog-details/blog-details.component.ts
+++ src/app/pages/main/blog-details/blog-details.component.ts
@@ -5,6 +5,8 @@ import {BlogService} from "~/core/api/blog.service";
 import {AsyncPipe, DatePipe, NgForOf, NgIf} from "@angular/common";
 import {LazyLoadImageModule} from "ng-lazyload-image";
 import {MarkdownPipe} from "ngx-markdown";
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-blog-details',
@@ -24,13 +26,16 @@ import {MarkdownPipe} from "ngx-markdown";
 export class BlogDetailsComponent implements OnInit {
   blog: BlogDto | undefined;
   markdownContent = '# Hello, Markdown!'
+  url: string='';
   constructor(
     private route: ActivatedRoute,
-    private blogService: BlogService
+    private blogService: BlogService,
+    private location: Location
   ) {
   }
 
   ngOnInit() {
+    this.url = window.location.origin + this.location.path();
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.blogService.getOne(id).subscribe({
@@ -43,6 +48,32 @@ export class BlogDetailsComponent implements OnInit {
       });
     }
   }
+
+  share(platform: string) {
+    let shareUrl = '';
+
+    switch (platform) {
+      case 'facebook':
+        shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(this.url)}`;
+        break;
+      case 'twitter':
+        shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(this.url)}&text=${encodeURIComponent(this.blog?.title || '')}`;
+        break;
+      case 'google':
+        shareUrl = `https://plus.google.com/share?url=${encodeURIComponent(this.url)}`;
+        break;
+      case 'linkedin':
+        shareUrl = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(this.url)}&title=${encodeURIComponent(this.blog?.title || '')}`;
+        break;
+    }
+
+    if (shareUrl) {
+      window.open(shareUrl, '_blank', 'width=600,height=400,left=100,top=100');
+    }
+  }
+
+
+ 
 
   protected readonly ContextType = ContextType;
 }
